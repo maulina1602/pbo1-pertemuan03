901d0a5c26f9bc33acd021d311c52664aa80460e
maulina1602 <linamaulina.ml@gmail.com>
2020-04-12 09:58:38 +0700

    Operator Binary Assignment modif

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -5,10 +5,15 @@ public class OperatorBinary {
     //operator (=) sebagai assignment/penugasan
     //baris 10-11 menempatkan nilai kedalam variabel
     //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+    public static void main(String[] args) {
+        String nama = "Maulina";
+        int JumlahSks = 24;
+        double IndeksPrestasiKumulatif = 3.89;
 
-    String nama = "Maulina";
-    int JumlahSks = 24;
-    double IndeksPrestasiKumulatif = 3.89;
-
+        System.out.println(nama);
+        System.out.println(JumlahSks);
+        System.out.println(IndeksPrestasiKumulatif);
+        
+    }
 
 }
