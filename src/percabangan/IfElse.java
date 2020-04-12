39a155fd07411ef6deac60a1449bdee119930b9e
maulina1602 <linamaulina.ml@gmail.com>
2020-04-12 13:29:07 +0700

    Lagi dlm keadaan pas pasan hiks corona :(

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,8 +2,8 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 25000;
-        double uangDiDompet = 65000;
+        double totalBelanja = 6000;
+        double uangDiDompet = 6000;
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, #DirumahAja");
