718348d19cf9a94e75506ad375db2d63be204133
maulina1602 <linamaulina.ml@gmail.com>
2020-04-12 13:52:13 +0700

    case 1 pandemi covid-19 hilang

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,24 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int bejalanan=1;            //lagi gabut jadi ulun commit satu2 ai pa ai wkwkwk
+        switch (bejalanan){
+            case 1:
+                System.out.println("urang lapas jr bapa :V");
+                break;
+            case 2:
+                System.out.println("urang acayan jr bapa <3");
+                break;
+            case 3:
+                System.out.println("ubat nyamuk wara :( kasian lo pa");
+                break;
+            case 4:
+                System.out.println("dubil dit muka indumarit :D");
+                break;
+            default:
+                System.out.println("makhluk halus sejenis kunti XDXD");
+                break;
+        }
+    }
+}
