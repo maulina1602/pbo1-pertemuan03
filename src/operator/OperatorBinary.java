3c9a1e1b92117a56e4ea3683ad0fcd0d23dda67f
maulina1602 <linamaulina.ml@gmail.com>
2020-04-12 09:22:14 +0700

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //baris 10-11 menempatkan nilai kedalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Maulina";
+    int JumlahSks = 24;
+    double IndeksPrestasiKumulatif = 3.89;
+
+
+}
